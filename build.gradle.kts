08f786dc0422ab7c2b472b6408503bc38ffe14e0
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2019-01-30 21:41:42 -0700

    Fix nebula bintray version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     id("nebula.nebula-bintray") version "3.5.2"
     id("nebula.publish-verification") version "9.4.6"
     id("nebula.source-jar") version "9.4.6"
-    id("nebula.nebula-bintray-publishing") version "9.4.6"
+    id("nebula.nebula-bintray-publishing") version "5.0.0"
 }
 
 group = "com.tylerthrailkill.helpers"
