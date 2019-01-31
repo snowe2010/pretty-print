b747555c50c9b0e7d907d29e0eed5cdad8a792df
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2019-01-30 21:33:52 -0700

    Add nebula publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,17 @@
+import com.jfrog.bintray.gradle.BintrayExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val kotlin_version = "1.3.11"
 val spek_version = "2.0.0-alpha.2"
 
 plugins {
+    `build-scan`
     kotlin("jvm") version "1.3.11"
     id("nebula.maven-publish") version "9.4.6"
     id("nebula.nebula-bintray") version "3.5.2"
     id("nebula.publish-verification") version "9.4.6"
-    `build-scan`
+    id("nebula.source-jar") version "9.4.6"
+    id("nebula.nebula-bintray-publishing") version "9.4.6"
 }
 
 group = "com.tylerthrailkill.helpers"
@@ -26,7 +29,7 @@ dependencies {
     testCompile(kotlin("test"))
     testCompile(kotlin("test-junit"))
 
-    compile(group= "ch.qos.logback", name= "logback-classic", version= "1.3.0-alpha4")
+    compile(group = "ch.qos.logback", name = "logback-classic", version = "1.3.0-alpha4")
     compile(group = "org.slf4j", name = "slf4j-simple", version = "1.6.1")
     compile("io.github.microutils:kotlin-logging:1.6.22")
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
@@ -63,4 +66,31 @@ tasks {
 buildScan {
     termsOfServiceUrl = "https://gradle.com/terms-of-service"
     termsOfServiceAgree = "yes"
+}
+
+configure<BintrayExtension> {
+    user = findProperty("bintrayUser") as String? ?: System.getenv("BINTRAY_USER")
+    key = findProperty("bintrayKey") as String? ?: System.getenv("BINTRAY_KEY")
+    override = true
+
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        userOrg = "snowe"
+        repo = "maven"
+        name = "Pretty-Print"
+        desc = "Pretty printing of objects"
+
+        setLicenses("MIT")
+        websiteUrl = "https://github.com/snowe2010/${project.name}"
+        issueTrackerUrl = "https://github.com/snowe2010/${project.name}/issues"
+        vcsUrl = "https://github.com/snowe2010/${project.name}.git"
+        setLabels("axon", "kotlin")
+//        version(delegateClosureOf<BintrayExtension.VersionConfig> {
+//            gpg(delegateClosureOf<BintrayExtension.GpgConfig> {
+//                sign = true
+//                passphrase = findProperty("gpgPassphrase") as String? ?: System.getenv("GPG_PASSPHRASE")
+//            })
+//        })
+    })
+    dryRun = false
+    publish = true
 }
