3f9b4a726e3e79a76dce3ddeb177031f72e9087b
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2019-02-12 16:19:05 -0700

    Add more tests around nullability and tab spacing

--- src/test/kotlin/com/tylerthrailkill/helpers/prettyprint/NullabilityTest.kt
+++ src/test/kotlin/com/tylerthrailkill/helpers/prettyprint/NullabilityTest.kt
@@ -2,125 +2,32 @@ package com.tylerthrailkill.helpers.prettyprint
 
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
-import java.io.ByteArrayOutputStream
 
 object NullabilityTest : Spek({
     setup()
 
-    describe("pretty printing lists should") {
-        it("render a list of strings") {
-            prettyPrint(listOf("a", "b", "c")) mapsTo """
-                [
-                  "a",
-                  "b",
-                  "c"
-                ]
+//    describe("pretty printing lists should") {
+//        it("render a list of strings") {
+//            prettyPrint(Unit) mapsTo """
+//                [
+//                  "a",
+//                  "b",
+//                  "c"
+//                ]
+//                """
+//        }
+//    }
+    describe("pretty printing") {
+        it("null should work") {
+            prettyPrint(null) mapsTo """
+                null
                 """
         }
-        it("render a list of mixed primitive objects") {
-            prettyPrint(listOf("a", 1, true)) mapsTo """
-                [
-                  "a",
-                  1,
-                  true
-                ]
+        it("objects with no fields should work") {
+            prettyPrint(EmptyObject()) mapsTo """
+                EmptyObject(
+                )
                 """
-        }
-        it("render null lists as null") {
-            prettyPrint(NullableLists(null)) mapsTo """
-            NullableLists(
-              col = null
-            )
-            """
-        }
-        it("render lists with null items") {
-            prettyPrint(NullableLists(listOf(null, null, null))) mapsTo """
-            NullableLists(
-              col = [
-                      null,
-                      null,
-                      null
-                    ]
-            )
-            """
-        }
-        it("render lists with mixed null and not-null items") {
-            prettyPrint(NullableLists(listOf(null, 1, null, "a", null, true))) mapsTo """
-            NullableLists(
-              col = [
-                      null,
-                      1,
-                      null,
-                      "a",
-                      null,
-                      true
-                    ]
-            )
-            """
-        }
-        it("render lists with a small object") {
-            prettyPrint(NullableLists(listOf(TinyObject(1)))) mapsTo """
-            NullableLists(
-              col = [
-                      TinyObject(
-                        int = 1
-                      )
-                    ]
-            )
-            """
-        }
-        it("render lists with multiple small objects") {
-            prettyPrint(
-                NullableLists(
-                    listOf(
-                        TinyObject(1),
-                        TinyObject(2),
-                        TinyObject(3)
-                    )
-                )
-            ) mapsTo """
-            NullableLists(
-              col = [
-                      TinyObject(
-                        int = 1
-                      ),
-                      TinyObject(
-                        int = 2
-                      ),
-                      TinyObject(
-                        int = 3
-                      )
-                    ]
-            )
-            """
-        }
-        it("lists with maps") {
-            prettyPrint(
-                NullableLists(
-                    listOf(
-                        mapOf(1 to 2, 3 to 4),
-                        mapOf(5 to 6, 7 to 8),
-                        mapOf(9 to 0, 0 to 1)
-                    )
-                )
-            ) mapsTo """
-            NullableLists(
-              col = [
-                      {
-                        1 -> 2,
-                        3 -> 4
-                      },
-                      {
-                        5 -> 6,
-                        7 -> 8
-                      },
-                      {
-                        9 -> 0,
-                        0 -> 1
-                      }
-                    ]
-            )
-            """
         }
     }
 })
--- src/test/kotlin/com/tylerthrailkill/helpers/prettyprint/TabTest.kt
+++ src/test/kotlin/com/tylerthrailkill/helpers/prettyprint/TabTest.kt
@@ -19,5 +19,45 @@ object TabTest : Spek({
                 ]
                 """
         }
+        it("render a list of lists with 4 spaces for the tab size instead of 2") {
+            prettyPrint(
+                tabSize = 4,
+                obj = listOf(listOf("a", "b", "c"), listOf("a", "b", "c"), listOf("a", "b", "c"))
+            ) mapsTo """
+                [
+                    [
+                        "a",
+                        "b",
+                        "c"
+                    ],
+                    [
+                        "a",
+                        "b",
+                        "c"
+                    ],
+                    [
+                        "a",
+                        "b",
+                        "c"
+                    ]
+                ]
+                """
+        }
+        it("render a map with 4 spaces for the tab size instead of 2") {
+            prettyPrint(
+                tabSize = 4,
+                obj = mapOf(mapOf(1 to 2, 2 to 3) to mapOf(3 to 4, 4 to 5))
+            ) mapsTo """
+                {
+                    {
+                        1 -> 2,
+                        2 -> 3
+                    } -> {
+                        3 -> 4,
+                        4 -> 5
+                    }
+                }
+                """
+        }
     }
 })
