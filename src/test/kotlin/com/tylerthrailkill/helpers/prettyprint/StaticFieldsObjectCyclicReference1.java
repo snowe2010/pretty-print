7df5c9757e5219fa6e9124685a10f2049e5e4d6d
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2020-10-01 23:09:11 -0600

    Update more linter rules

--- src/test/kotlin/com/tylerthrailkill/helpers/prettyprint/StaticFieldsObjectCyclicReference1.java
+++ src/test/kotlin/com/tylerthrailkill/helpers/prettyprint/StaticFieldsObjectCyclicReference1.java
@@ -1,19 +1,11 @@
 package com.tylerthrailkill.helpers.prettyprint;
 
 public class StaticFieldsObjectCyclicReference1 {
-//    public static final StaticFieldsObjectCyclicReference1 FOO;
     public static final StaticFieldsObjectCyclicReference1 BAR;
-//    public static final StaticFieldsObject BAZ;
-//    public static final StaticFieldsObject BIZ;
-//    public static final StaticFieldsObject BOOZE;
     public StaticFieldsObject n;
     
     static {
-//        FOO = new StaticFieldsObjectCyclicReference1(BAR);
         BAR = new StaticFieldsObjectCyclicReference1(StaticFieldsObject.FOO);
-//        BAZ = new StaticFieldsObject(2);
-//        BIZ = new StaticFieldsObject(3);
-//        BOOZE = new StaticFieldsObject(4);
     }
 
     public StaticFieldsObjectCyclicReference1(StaticFieldsObject i) {
