66e033758e894bb6420e3263c85cbd3818de90ef
Tyler B. Thrailkill <tyler.b.thrailkill@gmail.com>
2020-10-03 11:32:40 -0600

    Don't set RELEASE_VERSION on snapshots
    * apparently it was being set to an empty string which caused the bug

--- buildSrc/src/main/kotlin/Ci.kt
+++ buildSrc/src/main/kotlin/Ci.kt
@@ -2,13 +2,8 @@
  * Used under Apache 2 License from https://github.com/kotest/kotest/blob/master/buildSrc/src/main/kotlin/Ci.kt
  */
 object Ci {
-
-   // this is the version used for building snapshots
-   // .buildnumber-snapshot will be appended
    private const val snapshotBase = "2.0.3"
-
    private val githubBuildNumber = System.getenv("GITHUB_RUN_NUMBER")
-
    private val snapshotVersion = when (githubBuildNumber) {
       null -> "$snapshotBase-LOCAL"
       else -> "$snapshotBase.${githubBuildNumber}-SNAPSHOT"
@@ -16,6 +11,6 @@ object Ci {
 
    private val releaseVersion = System.getenv("RELEASE_VERSION")
 
-   val isRelease = releaseVersion != null
+   val isRelease = releaseVersion != null && releaseVersion.isNotEmpty()
    val publishVersion = releaseVersion ?: snapshotVersion
 }
